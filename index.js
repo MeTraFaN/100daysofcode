400806f4b1bd72d1a98bd7ad3562042524d3e631
MeTraFaN <metrafan1@mail.ru>
2017-05-05 19:32:19 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var port = process.env.PORT || 3000;
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
-
+ 
 io.on('connection', function(socket){
   socket.on('button clicked', function(value){
     io.emit('chat message', value);
