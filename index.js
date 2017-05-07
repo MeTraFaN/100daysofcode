066b1d96beae842800fe51724d2f54da73041686
MeTraFaN <metrafan1@mail.ru>
2017-05-07 16:18:38 +0300

    Update index.js

--- index.js
+++ index.js
@@ -20,10 +20,24 @@ app.get('/day21', function(req, res){
 });
 var users = {};
 io.on('connection', function(client) {
-  client.on('soloanim', function(value){
-     client.emit('soloanim',  value);
+  client.emit('users base', users);
+  client.emit('user connected', client.id);
+  client.on('user done', function(coordx, coordy){
+    users[client.id] = {
+      x: coordx,
+      y: coordy
+    }
+    client.broadcast.emit('user done', coordx, coordy, client.id)
   });
-  
+  client.on('button clicked', function(value){
+    client.broadcast.emit('sprite change coord', client.id, value);
+    client.emit('button clicked', client.id, value);
+    users[client.id].x = +users[client.id].x + value
+  });
+  client.on('disconnect', function(){
+    client.broadcast.emit('user disconnected', client.id);
+    delete users[client.id];
+  });
 });
 
 http.listen(port, function(){
