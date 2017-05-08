545be8143b10eddae2a56347f1e1146406343432
MeTraFaN <metrafan1@mail.ru>
2017-05-08 23:11:41 +0300

    Update index.js

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ io.on('connection', function(client) {
     client.broadcast.emit('user done', coordx, coordy, client.id)
   });
   client.on('move done', function(obj){
-    client.broadcast.emit('sprite change coord', client.id,  obj);
+    client.broadcast.emit('sprite change coord',  client.id,  obj, users);
     client.emit('move done', client.id, obj);
     //users[client.id].x = +users[client.id].x + obj.x;
     //users[client.id].y = +users[client.id].y + obj.y;
