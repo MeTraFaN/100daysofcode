d6ad65efc1ef9b2051fab45e4e5fd8b078f57df1
MeTraFaN <metrafan1@mail.ru>
2017-05-16 22:48:25 +0300

    Update index.js

--- index.js
+++ index.js
@@ -45,7 +45,8 @@ io.on('connection', function(client) {
     client.emit('user connected30', client.id, boardname, board30); 
   });
   client.on('board join', function(boardname){
-      if (boardname in board30){
+      client.emit('test', "yes");
+    /*if (boardname in board30){
         //client.join(boardname);
         //client.emit('test', "yes");
         //board30[boardname] = {Clients: client.id};
@@ -53,7 +54,8 @@ io.on('connection', function(client) {
       }
       else {
         socket.emit('board errore', boardname)
-      }      
+      }  */
+    
   });
   client.on('user done30', function (x, y, color, size, boardname){
     board30[boardname].Clients = {
