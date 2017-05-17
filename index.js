1e46b1d1f39724e43626483cb19027c3ba6fcba9
MeTraFaN <metrafan1@mail.ru>
2017-05-17 11:02:05 +0300

    Update index.js

--- index.js
+++ index.js
@@ -61,7 +61,7 @@ io.on('connection', function(client) {
     }   
   });
   client.on('user done30', function (x, y, color, size, boardname){
-    board30[boardname] = {
+    board30[boardname] : {
       [client.id]: {
                     x: x, 
                     y: y,
