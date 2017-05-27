119ee20482908f6ca1c9570d946fb58b77ca002b
MeTraFaN <metrafan1@mail.ru>
2017-05-27 23:41:19 +0300

    Update second.js

--- js/second.js
+++ js/second.js
@@ -1,4 +1,5 @@
 var ThisCell, CellHeight;
+var arr = new Array(9);
 var canvas = document.getElementById('canvas');
 CWidth = window.innerWidth;
 CHeight = window.innerHeight;
@@ -7,7 +8,7 @@ canvas.style.height = CHeight + 'px';
 for (var i=0; i < 9; i++){
 	divCell = document.createElement('div');
 	divCell.className = "cell";
-	divCell.id = "cell" + i +"";
+	divCell.id = i +"";
 	divCell.setAttribute("onClick", "TakeElement(this.id)");
 	divCell.setAttribute("ontouchend", "TakeElement(this.id)");
 	divCell.style.width = CWidth / 3 - 35 + 'px';
@@ -21,13 +22,14 @@ function TakeElement(val){
 	ThisCell = document.getElementById(val);
 	CellHeight = ThisCell.style.height;
 	if(X){	
-		WriteElement("X",false);
+		WriteElement("X",false, val);
 	}
 	else if(!X){
 		WriteElement("0",true);
 	}
+	isitend();
 };
-function WriteElement (first, second){
+function WriteElement (first, second, id){
 	if (!ThisCell.firstElementChild){
 		ThisCell.innerHTML = "<div class='Value'>" + first + "</div>";
 		Value = ThisCell.firstElementChild;
@@ -35,5 +37,17 @@ function WriteElement (first, second){
 		Value.style.fontSize = parseInt(CellHeight, 10) - 30 + 'px';
 		X = second;
 		return 0;
+		arr[i] = first;
 	}
+}
+function isitend(id){
+	if (arr[0]=='X' && arr[1]=='X' && arr[2]=='X' || arr[0]=='0' && arr[1]=='0' && arr[2]=='0')  return true;
+  	if (arr[3]=='X' && arr[4]=='X' && arr[5]=='X' || arr[3]=='0' && arr[4]=='0' && arr[5]=='0')  return true;
+  	if (arr[6]=='X' && arr[7]=='X' && arr[8]=='X' || arr[6]=='0' && arr[7]=='0' && arr[8]=='0')  return true;
+  	if (arr[0]=='X' && arr[3]=='X' && arr[6]=='X' || arr[0]=='0' && arr[3]=='0' && arr[6]=='0')  return true;
+  	if (arr[1]=='X' && arr[4]=='X' && arr[7]=='X' || arr[1]=='0' && arr[4]=='0' && arr[7]=='0')  return true;
+  	if (arr[2]=='X' && arr[5]=='X' && arr[8]=='X' || arr[2]=='0' && arr[5]=='0' && arr[8]=='0')  return true;
+  	if (arr[0]=='X' && arr[4]=='X' && arr[8]=='X' || arr[0]=='0' && arr[4]=='0' && arr[8]=='0')  return true;
+  	if (arr[2]=='X' && arr[4]=='X' && arr[6]=='X' || arr[2]=='0' && arr[4]=='0' && arr[6]=='0')  return true;
+  	if(arr[0] && arr[1] && arr[2] && arr[3] && arr[4] && arr[5] && arr[6] && arr[7] && arr[8]) return true;
 }
