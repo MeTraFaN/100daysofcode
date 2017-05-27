d1ec50e1ddeedff53eed1d9ef1d0cec71c8d7d8b
MeTraFaN <metrafan1@mail.ru>
2017-05-27 23:57:11 +0300

    Update second.js

--- js/second.js
+++ js/second.js
@@ -27,7 +27,7 @@ function TakeElement(val){
 	else if(!X){
 		WriteElement("0",true);
 	}
-	isitend();
+	isitend() ? reset();
 };
 function WriteElement (first, second, id){
 	if (!ThisCell.firstElementChild){
@@ -50,4 +50,10 @@ function isitend(id){
   	if (arr[0]=='X' && arr[4]=='X' && arr[8]=='X' || arr[0]=='0' && arr[4]=='0' && arr[8]=='0')  return true;
   	if (arr[2]=='X' && arr[4]=='X' && arr[6]=='X' || arr[2]=='0' && arr[4]=='0' && arr[6]=='0')  return true;
   	if(arr[0] && arr[1] && arr[2] && arr[3] && arr[4] && arr[5] && arr[6] && arr[7] && arr[8]) return true;
+}
+
+function reset(){
+  alert("Игра окончена!");
+  location.reload();
+
 }
