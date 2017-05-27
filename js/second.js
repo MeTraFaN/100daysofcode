b702b9c6a7b41bdeb5cb63cfffd1d58c8b9519fe
MeTraFaN <metrafan1@mail.ru>
2017-05-28 02:03:57 +0300

    Update second.js

--- js/second.js
+++ js/second.js
@@ -37,18 +37,27 @@ function WriteElement (first, second, id){
 		arr[id] = first;
 		console.log(arr);
 	}
-        if (isitend()) { reset()};
+	console.log(isitend());
+       // if (isitend() ) { reset()};
 }
 function isitend(id){
-	if (arr[0]=='X' && arr[1]=='X' && arr[2]=='X' || arr[0]=='O' && arr[1]=='O' && arr[2]=='O')  return true;
-  	if (arr[3]=='X' && arr[4]=='X' && arr[5]=='X' || arr[3]=='O' && arr[4]=='O' && arr[5]=='O')  return true;
-  	if (arr[6]=='X' && arr[7]=='X' && arr[8]=='X' || arr[6]=='O' && arr[7]=='O' && arr[8]=='O')  return true;
-  	if (arr[0]=='X' && arr[3]=='X' && arr[6]=='X' || arr[0]=='O' && arr[3]=='O' && arr[6]=='O')  return true;
-  	if (arr[1]=='X' && arr[4]=='X' && arr[7]=='X' || arr[1]=='O' && arr[4]=='O' && arr[7]=='O')  return true;
-  	if (arr[2]=='X' && arr[5]=='X' && arr[8]=='X' || arr[2]=='O' && arr[5]=='O' && arr[8]=='O')  return true;
-  	if (arr[0]=='X' && arr[4]=='X' && arr[8]=='X' || arr[0]=='O' && arr[4]=='O' && arr[8]=='O')  return true;
-  	if (arr[2]=='X' && arr[4]=='X' && arr[6]=='X' || arr[2]=='O' && arr[4]=='O' && arr[6]=='O')  return true;
-  	if (arr[0] && arr[1] && arr[2] && arr[3] && arr[4] && arr[5] && arr[6] && arr[7] && arr[8]) return true;
+	if (arr[0]=='X' && arr[1]=='X' && arr[2]=='X' || 
+	    arr[3]=='X' && arr[4]=='X' && arr[5]=='X' ||
+	    arr[6]=='X' && arr[7]=='X' && arr[8]=='X' ||
+	    arr[0]=='X' && arr[3]=='X' && arr[6]=='X' ||
+	    arr[1]=='X' && arr[4]=='X' && arr[7]=='X' ||
+	    arr[2]=='X' && arr[5]=='X' && arr[8]=='X' ||
+	    arr[0]=='X' && arr[4]=='X' && arr[8]=='X' ||
+	    arr[2]=='X' && arr[4]=='X' && arr[6]=='X') return 'X';
+	if (arr[0]=='O' && arr[1]=='O' && arr[2]=='O' ||
+	    arr[3]=='O' && arr[4]=='O' && arr[5]=='O' ||
+	    arr[6]=='O' && arr[7]=='O' && arr[8]=='O' ||
+	    arr[0]=='O' && arr[3]=='O' && arr[6]=='O' ||
+	    arr[1]=='O' && arr[4]=='O' && arr[7]=='O' ||
+	    arr[2]=='O' && arr[5]=='O' && arr[8]=='O' ||
+	    arr[0]=='O' && arr[4]=='O' && arr[8]=='O' ||
+	    arr[2]=='O' && arr[4]=='O' && arr[6]=='O') return 'O';
+	if (arr[0] && arr[1] && arr[2] && arr[3] && arr[4] && arr[5] && arr[6] && arr[7] && arr[8]) return 'draw';   	 
 }
 
 function reset(){
