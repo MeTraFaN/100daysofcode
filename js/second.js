852bc17edc4665932d5e10039d419b5e2e3a122d
MeTraFaN <metrafan1@mail.ru>
2017-05-28 00:52:43 +0300

    Update second.js

--- js/second.js
+++ js/second.js
@@ -24,7 +24,7 @@ function TakeElement(val){
 		WriteElement("X",false, val);
 	}
 	else if(!X){
-		WriteElement("0",true);
+		WriteElement("O",true);
 	}	
 };
 function WriteElement (first, second, id){
@@ -39,14 +39,14 @@ function WriteElement (first, second, id){
         if (isitend()) { reset()};
 }
 function isitend(id){
-	if (arr[0]=='X' && arr[1]=='X' && arr[2]=='X' || arr[0]=='0' && arr[1]=='0' && arr[2]=='0')  return true;
-  	if (arr[3]=='X' && arr[4]=='X' && arr[5]=='X' || arr[3]=='0' && arr[4]=='0' && arr[5]=='0')  return true;
-  	if (arr[6]=='X' && arr[7]=='X' && arr[8]=='X' || arr[6]=='0' && arr[7]=='0' && arr[8]=='0')  return true;
-  	if (arr[0]=='X' && arr[3]=='X' && arr[6]=='X' || arr[0]=='0' && arr[3]=='0' && arr[6]=='0')  return true;
-  	if (arr[1]=='X' && arr[4]=='X' && arr[7]=='X' || arr[1]=='0' && arr[4]=='0' && arr[7]=='0')  return true;
-  	if (arr[2]=='X' && arr[5]=='X' && arr[8]=='X' || arr[2]=='0' && arr[5]=='0' && arr[8]=='0')  return true;
-  	if (arr[0]=='X' && arr[4]=='X' && arr[8]=='X' || arr[0]=='0' && arr[4]=='0' && arr[8]=='0')  return true;
-  	if (arr[2]=='X' && arr[4]=='X' && arr[6]=='X' || arr[2]=='0' && arr[4]=='0' && arr[6]=='0')  return true;
+	if (arr[0]=='X' && arr[1]=='X' && arr[2]=='X' || arr[0]=='O' && arr[1]=='O' && arr[2]=='O')  return true;
+  	if (arr[3]=='X' && arr[4]=='X' && arr[5]=='X' || arr[3]=='O' && arr[4]=='O' && arr[5]=='O')  return true;
+  	if (arr[6]=='X' && arr[7]=='X' && arr[8]=='X' || arr[6]=='O' && arr[7]=='O' && arr[8]=='O')  return true;
+  	if (arr[0]=='X' && arr[3]=='X' && arr[6]=='X' || arr[0]=='O' && arr[3]=='O' && arr[6]=='O')  return true;
+  	if (arr[1]=='X' && arr[4]=='X' && arr[7]=='X' || arr[1]=='O' && arr[4]=='O' && arr[7]=='O')  return true;
+  	if (arr[2]=='X' && arr[5]=='X' && arr[8]=='X' || arr[2]=='O' && arr[5]=='O' && arr[8]=='O')  return true;
+  	if (arr[0]=='X' && arr[4]=='X' && arr[8]=='X' || arr[0]=='O' && arr[4]=='O' && arr[8]=='O')  return true;
+  	if (arr[2]=='X' && arr[4]=='X' && arr[6]=='X' || arr[2]=='O' && arr[4]=='O' && arr[6]=='O')  return true;
   	if (arr[0] && arr[1] && arr[2] && arr[3] && arr[4] && arr[5] && arr[6] && arr[7] && arr[8]) return true;
 }
 
