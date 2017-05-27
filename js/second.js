44f8627fd9a0d7342919ec0720ab959db4b06daf
MeTraFaN <metrafan1@mail.ru>
2017-05-27 20:58:35 +0300

    Update second.js

--- js/second.js
+++ js/second.js
@@ -15,6 +15,7 @@ for (var i=0; i < 9; i++){
 }
 var Cells = document.getElementsByClassName('cell');
 var X = true;
+Cells.addEventListener('ontouchend', TakeElement(this.id));
 
 function TakeElement(val){
 	ThisCell = document.getElementById(val);
